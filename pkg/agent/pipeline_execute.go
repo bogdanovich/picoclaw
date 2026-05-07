4c5595bf55ea60de64abe86823140c29552f4280
Anton Bogdanovich <27antonb@gmail.com>
2026-05-07 11:24:01 -0700

    fix(tools): allow relative script paths in exec guard

--- pkg/agent/pipeline_execute.go
+++ pkg/agent/pipeline_execute.go
@@ -557,6 +557,7 @@ toolLoop:
 			ts.opts.Dispatch.MessageID(),
 			ts.opts.Dispatch.ReplyToMessageID(),
 		)
+		execCtx = tools.WithToolTopicID(execCtx, originTopicID(ts.opts.Dispatch.InboundContext))
 		execCtx = tools.WithToolSessionContext(
 			execCtx,
 			ts.agent.ID,
